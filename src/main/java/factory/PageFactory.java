935489e1e277b30a4bbcb1a4dafe83a83600f57e
Dhruv Aggarwal <dhruv.aggarwal@gspann.com>
2025-07-31 19:35:57 +0530

    Added feature files and test scripts

--- src/main/java/factory/PageFactory.java
+++ src/main/java/factory/PageFactory.java
@@ -1,9 +1,11 @@
 package factory;
 
+import pages.ForgetPasswordPage;
 import pages.LoginPage;
 public class PageFactory {
     static ThreadLocal<LoginPage> LoginPageTL = new ThreadLocal<LoginPage>();
     //static ThreadLocal<HomePage> HomePageTL = new ThreadLocal<HomePage>();
+    static ThreadLocal<ForgetPasswordPage> ForgetPasswordPageTL = new ThreadLocal<ForgetPasswordPage>();
 
     public static LoginPage getLoginPage() {
         if(LoginPageTL.get()==null) {
@@ -12,6 +14,12 @@ public class PageFactory {
         return LoginPageTL.get();
     }
 
+    public static ForgetPasswordPage getForgetPasswordPage() {
+        if(ForgetPasswordPageTL.get()==null) {
+        	ForgetPasswordPageTL.set(new ForgetPasswordPage());
+        }
+        return ForgetPasswordPageTL.get();
+    }
     /*public static HomePage getHomePage() {
         if(HomePageTL.get()==null) {
             HomePageTL.set(new HomePage());
