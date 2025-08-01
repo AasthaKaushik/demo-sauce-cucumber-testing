f480ba121ba330ee0427e13224c6991c03c26034
AasthaKaushik <aastha.kaushik@gspann.com>
2025-08-01 07:22:05 +0530

    Added create account, login and home page page factories, feature files and step definitions

--- src/main/java/factory/DriverFactory.java
+++ src/main/java/factory/DriverFactory.java
@@ -2,6 +2,7 @@ package factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class DriverFactory {
     static ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
--- src/main/java/factory/PageFactory.java
+++ src/main/java/factory/PageFactory.java
@@ -1,10 +1,13 @@
 package factory;
 
+import pages.CreateAccountPage;
 import pages.ForgetPasswordPage;
+import pages.HomePage;
 import pages.LoginPage;
 public class PageFactory {
     static ThreadLocal<LoginPage> LoginPageTL = new ThreadLocal<LoginPage>();
-    //static ThreadLocal<HomePage> HomePageTL = new ThreadLocal<HomePage>();
+    static ThreadLocal<CreateAccountPage> CreateAccountTL = new ThreadLocal<CreateAccountPage>();
+    static ThreadLocal<HomePage> HomePageTL = new ThreadLocal<HomePage>();
     static ThreadLocal<ForgetPasswordPage> ForgetPasswordPageTL = new ThreadLocal<ForgetPasswordPage>();
 
     public static LoginPage getLoginPage() {
@@ -20,10 +23,16 @@ public class PageFactory {
         }
         return ForgetPasswordPageTL.get();
     }
-    /*public static HomePage getHomePage() {
+    public static CreateAccountPage getCreateAccountPage() {
+        if(CreateAccountTL.get()==null) {
+            CreateAccountTL.set(new CreateAccountPage());
+        }
+        return CreateAccountTL.get();
+    }
+    public static HomePage getHomePage() {
         if(HomePageTL.get()==null) {
             HomePageTL.set(new HomePage());
         }
         return HomePageTL.get();
-    }*/
+    }
 }
